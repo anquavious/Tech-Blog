406b3ee1bcb9b9e5630238dc73010a978dbc60d0
Anquavious Grant <agrant022@gmail.com>
2022-12-19 04:44:32 -0500

    updated commentjs

--- models/index.js
+++ models/index.js
@@ -2,36 +2,34 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+
 User.hasMany(Post, {
     foreignKey: 'user_id'
-});
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'cascade',
-    hooks:true
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: 'cascade',
-    hooks: true
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'cascade',
-    hooks:true
-});
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'cascade',
-    hooks:true
 })
 
-// Export the modules
-module.exports = { User, Post, Comment };
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
